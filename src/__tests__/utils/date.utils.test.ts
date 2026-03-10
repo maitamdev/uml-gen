1f1a85b37598774c9cc9a72362c7ea6079e196ef
maitamdev <maitamit062005@gmail.com>
2026-03-11 00:38:24 +0700

    test(utils): add date formatting utility tests

---
+++ src/__tests__/utils/date.utils.test.ts
@@ -0,0 +1,9 @@
+﻿// Date utility tests
+import { describe, it, expect } from 'vitest';
+import { formatDuration } from '../../utils/date.utils';
+
+describe('formatDuration', () => {
+  it('formats milliseconds', () => { expect(formatDuration(500)).toBe('500ms'); });
+  it('formats seconds', () => { expect(formatDuration(2500)).toBe('2.5s'); });
+  it('formats exact second', () => { expect(formatDuration(1000)).toBe('1.0s'); });
+});
