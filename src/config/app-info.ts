180ba37b3cb40d3c26949a7a58977ead1bcfddfb
maitamdev <maitamit062005@gmail.com>
2026-03-11 00:59:56 +0700

    feat(config): add app version and metadata constants

---
+++ src/config/app-info.ts
@@ -0,0 +1,5 @@
+﻿export const APP_VERSION = '1.1.0';
+export const APP_NAME = 'UML Diagram Generator';
+export const APP_AUTHOR = 'MaiTamDev';
+export const APP_REPO = 'https://github.com/maitamdev/uml-gen';
+export const getAppInfo = () => ({ name: APP_NAME, version: APP_VERSION, author: APP_AUTHOR, repo: APP_REPO });
