6412a8eeed957aa8e3194175386018488386418f
maitamdev <maitamit062005@gmail.com>
2026-03-11 00:59:56 +0700

    feat(pwa): add service worker for offline caching

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+﻿const CACHE_NAME = 'uml-gen-v1';
+const ASSETS = ['/', '/index.html'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))));
