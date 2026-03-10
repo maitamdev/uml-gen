ab4c7df74b7d078661537cf93e0bcaab9f201b5a
maitamdev <maitamit062005@gmail.com>
2026-03-11 00:58:42 +0700

    feat(state): add barrel exports for state management module

---
+++ src/state/index.ts
@@ -0,0 +1,5 @@
+﻿// State module barrel exports
+export { Store } from './store';
+export { appStore, initialState } from './app-state';
+export type { AppState } from './app-state';
+export * from './selectors';
