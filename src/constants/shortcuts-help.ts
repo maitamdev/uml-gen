2053e502d5715be396b2a52834e2782b2bbe3c27
maitamdev <maitamit062005@gmail.com>
2026-03-11 00:59:57 +0700

    feat(constants): add keyboard shortcut help data

---
+++ src/constants/shortcuts-help.ts
@@ -0,0 +1,8 @@
+﻿export const SHORTCUT_HELP = [
+  { keys: ['Ctrl', 'Enter'], action: 'Generate diagrams' },
+  { keys: ['Ctrl', 'Shift', 'C'], action: 'Copy code' },
+  { keys: ['Ctrl', 'Shift', 'P'], action: 'Export PNG' },
+  { keys: ['Ctrl', 'Shift', 'S'], action: 'Export SVG' },
+  { keys: ['Esc'], action: 'Close / Exit fullscreen' },
+];
+export const formatShortcut = (keys: string[]) => keys.join(' + ');
