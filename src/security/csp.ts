ce960c56fc7996ce345a1e95cde0359dd8fa8a5b
maitamdev <maitamit062005@gmail.com>
2026-03-11 00:58:39 +0700

    security: add Content Security Policy helper functions

---
+++ src/security/csp.ts
@@ -0,0 +1,18 @@
+﻿// Content Security Policy helpers
+export function generateCSPMeta(): string {
+  return [
+    "default-src 'self'",
+    "script-src 'self' 'unsafe-inline'",
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+    "font-src 'self' https://fonts.gstatic.com",
+    "connect-src 'self' https://router.huggingface.co https://api.groq.com",
+    "img-src 'self' data: blob:",
+  ].join('; ');
+}
+
+export function injectCSPMeta(): void {
+  const meta = document.createElement('meta');
+  meta.httpEquiv = 'Content-Security-Policy';
+  meta.content = generateCSPMeta();
+  document.head.prepend(meta);
+}
