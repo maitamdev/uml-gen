61fbde8c3a71ad5b3fc8e3cbcc9ebf423d07c273
maitamgk <maitamit062005@gmail.com>
2026-03-18 23:26:13 +0700

    feat: add restaurant management template

---
+++ src/templates/restaurant.ts
@@ -0,0 +1,4 @@
+﻿export const restaurantTemplate = {
+  name: 'Quáº£n lÃ½ nhÃ  hÃ ng',
+  description: 'Há»‡ thá»‘ng quáº£n lÃ½ nhÃ  hÃ ng: Ä‘áº·t bÃ n, gá»i mÃ³n, thanh toÃ¡n, quáº£n lÃ½ nguyÃªn liá»‡u',
+};
