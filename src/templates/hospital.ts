f51f0421ed4c4805fd18c6883d370815c4343994
maitamgk <maitamit062005@gmail.com>
2026-03-18 23:26:13 +0700

    feat: add hospital management template with all diagram types

---
+++ src/templates/hospital.ts
@@ -0,0 +1,4 @@
+﻿export const hospitalTemplate = {
+  name: 'Quáº£n lÃ½ bá»‡nh viá»‡n',
+  description: 'Há»‡ thá»‘ng quáº£n lÃ½ bá»‡nh viá»‡n: Ä‘áº·t lá»‹ch khÃ¡m, quáº£n lÃ½ bá»‡nh nhÃ¢n, bÃ¡c sÄ©, kÃª Ä‘Æ¡n thuá»‘c',
+};
