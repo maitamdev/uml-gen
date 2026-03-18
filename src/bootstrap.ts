5d6e703a1c65da37b036c4eafe89c886646fb77c
maitamgk <maitamit062005@gmail.com>
2026-03-18 23:28:19 +0700

    feat: add app bootstrap module combining all initializers

---
+++ src/bootstrap.ts
@@ -0,0 +1,12 @@
+﻿import { setupGlobalErrorHandler } from './error-boundary';
+import { initTheme } from './theme-manager';
+import { logVersion } from './version';
+import { injectPrintStyles } from './print-styles';
+
+export function bootstrapApp(): void {
+  setupGlobalErrorHandler();
+  initTheme();
+  logVersion();
+  injectPrintStyles();
+  console.log('[App] Bootstrapped successfully');
+}
