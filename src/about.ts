8f9cd846df07e86073825b429591258879a91c89
maitamgk <maitamit062005@gmail.com>
2026-03-18 23:30:33 +0700

    feat: add about dialog with app info display

---
+++ src/about.ts
@@ -0,0 +1,7 @@
+﻿export function getAppInfo(): { name: string; version: string; commit: string } {
+  return { name: 'UML Generator', version: '1.2.0', commit: 'latest' };
+}
+export function showAboutDialog(): void {
+  const info = getAppInfo();
+  console.log(info.name + ' v' + info.version);
+}
