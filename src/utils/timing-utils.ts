ef3597b801a154d659ac4d4a58865708af93adfc
maitamgk <maitamit062005@gmail.com>
2026-03-18 23:22:39 +0700

    feat: add debounce, throttle and delay timing utilities

---
+++ src/utils/timing-utils.ts
@@ -0,0 +1,9 @@
+﻿export function debounce<T extends (...args: any[]) => any>(fn: T, delay: number): (...args: Parameters<T>) => void {
+  let tid: ReturnType<typeof setTimeout>;
+  return (...args) => { clearTimeout(tid); tid = setTimeout(() => fn(...args), delay); };
+}
+export function throttle<T extends (...args: any[]) => any>(fn: T, limit: number): (...args: Parameters<T>) => void {
+  let inThrottle = false;
+  return (...args) => { if (!inThrottle) { fn(...args); inThrottle = true; setTimeout(() => { inThrottle = false; }, limit); } };
+}
+export function delay(ms: number): Promise<void> { return new Promise(r => setTimeout(r, ms)); }
