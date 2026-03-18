4babae80bebf3f886066235e4c1eb455058a91fc
maitamgk <maitamit062005@gmail.com>
2026-03-18 23:26:14 +0700

    feat: add school management template

---
+++ src/templates/school.ts
@@ -0,0 +1,4 @@
+﻿export const schoolTemplate = {
+  name: 'Quáº£n lÃ½ trÆ°á»ng há»c',
+  description: 'Há»‡ thá»‘ng quáº£n lÃ½ trÆ°á»ng há»c: Ä‘iá»ƒm danh, thá»i khÃ³a biá»ƒu, sá»• liÃªn láº¡c phá»¥ huynh',
+};
