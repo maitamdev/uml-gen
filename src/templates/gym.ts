edbe3171be3388d3aa3add52916bc4aa19be8316
maitamgk <maitamit062005@gmail.com>
2026-03-18 23:26:13 +0700

    feat: add gym membership management template

---
+++ src/templates/gym.ts
@@ -0,0 +1,4 @@
+﻿export const gymTemplate = {
+  name: 'Quáº£n lÃ½ phÃ²ng gym',
+  description: 'Há»‡ thá»‘ng quáº£n lÃ½ phÃ²ng gym: Ä‘Äƒng kÃ½ há»™i viÃªn, Ä‘áº·t lá»‹ch táº­p, theo dÃµi tiáº¿n Ä‘á»™',
+};
