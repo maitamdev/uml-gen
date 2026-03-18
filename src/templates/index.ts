ea9a86910f0b97a4a99cce91c3802634a58ae8bf
maitamgk <maitamit062005@gmail.com>
2026-03-18 23:26:14 +0700

    chore: add templates barrel export

---
+++ src/templates/index.ts
@@ -0,0 +1,5 @@
+﻿export { hospitalTemplate } from './hospital';
+export { restaurantTemplate } from './restaurant';
+export { parkingTemplate } from './parking';
+export { gymTemplate } from './gym';
+export { schoolTemplate } from './school';
